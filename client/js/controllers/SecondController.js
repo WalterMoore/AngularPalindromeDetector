2c2e7872db57a3a2cd8d8ecf64fff87e73d0123a
Walter Moore <walteragnewmoore@gmail.com>
2015-11-23 17:20:34 -0600

    buttons work

--- client/js/controllers/SecondController.js
+++ client/js/controllers/SecondController.js
@@ -1,4 +1,7 @@
-angular.module('PalindromeApp').controller('SecondController', function($scope){
+angular.module('PalindromeApp').controller('SecondController', ['$scope', '$location', function ($scope, $location){
 	 	$scope.message = "This is the Second View"
 		console.log("Inside the SecondController");
- });
+		$scope.returnToWelcome = function() {
+		$location.path('/welcome');
+	};
+ }]);
--- client/js/controllers/WelcomeController.js
+++ client/js/controllers/WelcomeController.js
@@ -1,4 +1,7 @@
-angular.module('PalindromeApp').controller('WelcomeController', function($scope){
+angular.module('PalindromeApp').controller('WelcomeController', ['$scope', '$location', function ($scope, $location){ 
  	$scope.message ="This is the Welcome View"
- 	console.log("inside WelcomeController")
-});
+	console.log("inside WelcomeController")
+		$scope.continueToSecond = function() {
+		$location.path('/second');
+	};
+}]);
