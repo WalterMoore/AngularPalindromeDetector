34c3e047ad9164e3e919b82d1c9ffa25f5f6d012
Walter Moore <walteragnewmoore@gmail.com>
2015-11-25 11:00:42 -0600

    wrapped it all inside a function

--- client/js/app.js
+++ client/js/app.js
@@ -1,21 +1,25 @@
-var app = angular.module('PalindromeApp', ['ngRoute']);
+(function() {
 
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: "WelcomeController",
-            templateUrl: "views/welcome.html"
-        })
-        .when('/welcome', {
-            controller: "WelcomeController",
-            templateUrl: "views/welcome.html"
-        })
-        .when('/second/:forwardWord', {
-            controller: 'SecondController',
-            templateUrl: 'views/second.html'
-        })
-        .otherwise({
-            redirectTo: '/welcome'
-        })
-    console.log("inside routing");
-}]);
+    var app = angular.module('PalindromeApp', ['ngRoute']);
+
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: "WelcomeController",
+                templateUrl: "views/welcome.html"
+            })
+            .when('/welcome', {
+                controller: "WelcomeController",
+                templateUrl: "views/welcome.html"
+            })
+            .when('/second/:forwardWord', {
+                controller: 'SecondController',
+                templateUrl: 'views/second.html'
+            })
+            .otherwise({
+                redirectTo: '/welcome'
+            })
+        console.log("inside routing");
+    }]);
+
+})();
